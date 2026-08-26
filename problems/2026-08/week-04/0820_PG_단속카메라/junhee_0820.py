00f89ab3ea53d2a69ec267afe3b96ed8746f2005
Junhee-lee <sk5514224@gmail.com>
2026-08-27 00:27:38 +0900

    solve: junhee 0820 단속카메라

---
+++ problems/2026-08/week-04/0820_PG_단속카메라/junhee_0820.py
@@ -0,0 +1,13 @@
+def solution(routes):
+    answer = 1
+    routes.sort(key=lambda x: x[1])
+    camera = routes[0][1]
+
+    for route in routes:
+        if route[0] <= camera <= route[1]:
+            continue
+        else:
+            camera = route[1]
+            answer += 1
+
+    return answer
